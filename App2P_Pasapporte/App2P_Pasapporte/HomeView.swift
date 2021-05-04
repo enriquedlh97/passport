1e1c1b096b6f21f32b726e7a38a1171acab6339b
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-04 09:27:21 -0700

    Update

--- App2P_Pasapporte/App2P_Pasapporte/HomeView.swift
+++ App2P_Pasapporte/App2P_Pasapporte/HomeView.swift
@@ -3,6 +3,7 @@
 //  App2P_Pasapporte
 //
 //  Created by Karla Perales Lopez on 02/05/21.
+//  Updated by Enrique Diaz de Leon Hicks on 04/05/2021
 //
 // PÁGINA DE INICIO
 
@@ -14,38 +15,32 @@ struct HomeView: View {
     @StateObject var media = MediaModel()
     
     var body: some View {
-            
-            NavigationView {
-                TabView {
+        
+        NavigationView {
+            TabView {
+                
+                ListView(media: media)
+                    .tabItem {
+                        Label("Countries", systemImage: "pin.circle.fill")
+                    }
+                VisitView(media: media)
+                    .tabItem {
+                        Label("Visited", systemImage: "checkmark.circle.fill")
                         
-//                    Text("")
-//                    .padding()
-//                    .background(
-//                    Image("H_BG-5")
-//                        .scaleEffect(x: 0.55, y: 0.55, anchor: .center)
-//                    )
-                    ListView(media: media)
-                        .tabItem {
-                            Label("Countries", systemImage: "pin.circle.fill")
-                        }
-                    VisitView(media: media)
-                                            .tabItem {
-                                                Label("Visited", systemImage: "checkmark.circle.fill")
-                                                    
-                                            }
-                    WeatherView(media: media)
-                        .tabItem {
-                            Label("Weather", systemImage: "sun.max.fill")
-                        }
-                    
-                }
+                    }
+                WeatherView(media: media)
+                    .tabItem {
+                        Label("Weather", systemImage: "sun.max.fill")
+                    }
                 
             }
-            .accentColor(Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)))
-            .onAppear {
-                UITabBar.appearance().barTintColor = UIColor(Color(#colorLiteral(red: 0.9986861348, green: 0.8718172312, blue: 0.3506337404, alpha: 1)))
-                
-            }
+            
+        }
+        .accentColor(Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)))
+        .onAppear {
+            UITabBar.appearance().barTintColor = UIColor(Color(#colorLiteral(red: 0.9986861348, green: 0.8718172312, blue: 0.3506337404, alpha: 1)))
+            
+        }
         
     }
 }
