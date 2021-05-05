05ae340e7c9093f8800121c8967a4b8c8d7b0639
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-05 14:53:55 -0700

    Adds defaultCountry extension

--- App2P_Pasapporte/App2P_Pasapporte/Extensions.swift
+++ App2P_Pasapporte/App2P_Pasapporte/Extensions.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+extension Country {
+    
+    public static var defaultCountry = Country(id: <#T##UUID#>, name: "Algeria", alpha2Code: "DZ", alpha3Code: "DZA", subregion: "Northern Africa", population: 37900000, lat: 28, long: 3)
+    
+}
+
 extension Media {
     
     public static var defaultMedia = Media(country: "France", city: "Paris", overview: "París, la capital de Francia, es una importante ciudad europea y un centro mundial del arte, la moda, la gastronomía y la cultura. Su paisaje urbano del siglo XIX está entrecruzado por amplios bulevares y el río Sena. Aparte de estos hitos, como la Torre Eiffel y la catedral gótica de Notre Dame del siglo XII, la ciudad es famosa por su cultura del café y las tiendas de moda de diseñador a lo largo de la calle Rue du Faubourg Saint-Honoré.",video: "https://youtu.be/UfEiKK-iX70", info: "https://es.wikipedia.org/wiki/Francia")
