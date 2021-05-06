7157c09ec776ef2409474b39438c5f99e1d9383a
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-05 19:44:24 -0700

    Update DataModel

--- App2P_Pasapporte/App2P_Pasapporte/DataModel.swift
+++ App2P_Pasapporte/App2P_Pasapporte/DataModel.swift
@@ -13,13 +13,14 @@ import SwiftyJSON
 class DataModel: ObservableObject {
     
     @Published var africa: Region
-    @Published var americas: Region
-    @Published var asia: Region
-    @Published var europe: Region
-    @Published var oceania: Region
+//    @Published var americas: Region
+//    @Published var asia: Region
+//    @Published var europe: Region
+//    @Published var oceania: Region
     
     init() {
-        getData()
+        self.africa = Region(regionName: "africa", countriesList: loadRegionData(regionName: "africa"))
+        print(self.africa.countriesList)
     }
     
     func getData() {
@@ -33,60 +34,6 @@ class DataModel: ObservableObject {
         
     }
     
-    func loadRegionData(regionName: String) {
-        
-        var regionCountries = [Country]()
-        
-        let URL = "https://restcountries-v1.p.rapidapi.com/region/\(regionName)"
-        
-        // Makes request with specified parameters to get genres data
-        AF.request(URL, method: .get, encoding: URLEncoding.default, headers: HTTPHeaders(headers)).responseData { [self] data in
-            // Decodes the data saved in the data variable gotten by the .responseData
-            let json = try! JSON(data: data.data!)
-            
-            // Creates country variable to hold country-sepecific data
-            var country: Country
-            
-            //print(json[0]["name"])
-            // Loops over array to get and save the data
-            for item in json {
-                //print(item.1["latlng"][0])
-                
-                //self.loadFlag(iso3: item.1["alpha2Code"].stringValue)
-                
-                //                 Each item corresponds to a country. So, for each item, a Country object is created and the item's data is saved as the object's attributes. All items/countries of the same region are appended to the corresponding region property of the DataModel
-                country = Country(
-                    name: item.1["name"].stringValue,
-                    alpha2Code: item.1["alpha2Code"].stringValue,
-                    alpha3Code: item.1["alpha2Code"].stringValue,
-                    subregion: item.1["subregion"].stringValue,
-                    population: item.1["population"].doubleValue,
-                    lat: item.1["latlang"][0].floatValue,
-                    long: item.1["latlang"][1].floatValue,
-                    flag: "")
-                
-                // Once this is done, the object is appended to the Regions county property array
-                regionCountries.append(country)
-            }
-            
-            switch regionName {
-            case "africa":
-                self.africa = Region(regionName: regionName, countriesList: regionCountries)
-            case "americas":
-                self.americas = Region(regionName: regionName, countriesList: regionCountries)
-            case "asia":
-                self.asia = Region(regionName: regionName, countriesList: regionCountries)
-            case "europe":
-                self.europe = Region(regionName: regionName, countriesList: regionCountries)
-            case "oceania":
-                self.oceania = Region(regionName: regionName, countriesList: regionCountries)
-                
-            default:
-                print("None")
-            }
-        }
-        
-    }
     func loadFlag(iso3: String) {
         
         let url = "https://disease.sh/v3/covid-19/countries/countries/\(iso3)"
@@ -128,4 +75,60 @@ class DataModel: ObservableObject {
     }
 }
 
-
+func loadRegionData(regionName: String) -> [Country] {
+    
+    var regionCountries = [Country]()
+    
+    let URL = "https://restcountries-v1.p.rapidapi.com/region/\(regionName)"
+    
+    // Makes request with specified parameters to get genres data
+    AF.request(URL, method: .get, encoding: URLEncoding.default, headers: HTTPHeaders(headers)).responseData { data in
+        // Decodes the data saved in the data variable gotten by the .responseData
+        let json = try! JSON(data: data.data!)
+        
+        // Creates country variable to hold country-sepecific data
+        var country: Country
+        
+        //print(json[0]["name"])
+        // Loops over array to get and save the data
+        for item in json {
+            //print(item.1["latlng"][0])
+            
+            //self.loadFlag(iso3: item.1["alpha2Code"].stringValue)
+            
+            //                 Each item corresponds to a country. So, for each item, a Country object is created and the item's data is saved as the object's attributes. All items/countries of the same region are appended to the corresponding region property of the DataModel
+            country = Country(
+                name: item.1["name"].stringValue,
+                alpha2Code: item.1["alpha2Code"].stringValue,
+                alpha3Code: item.1["alpha2Code"].stringValue,
+                subregion: item.1["subregion"].stringValue,
+                population: item.1["population"].doubleValue,
+                lat: item.1["latlang"][0].floatValue,
+                long: item.1["latlang"][1].floatValue,
+                flag: "")
+            
+            // Once this is done, the object is appended to the Regions county property array
+            regionCountries.append(country)
+            
+        }
+        
+//            switch regionName {
+//            case "africa":
+//                self.africa = Region(regionName: regionName, countriesList: regionCountries)
+//            case "americas":
+//                self.americas = Region(regionName: regionName, countriesList: regionCountries)
+//            case "asia":
+//                self.asia = Region(regionName: regionName, countriesList: regionCountries)
+//            case "europe":
+//                self.europe = Region(regionName: regionName, countriesList: regionCountries)
+//            case "oceania":
+//                self.oceania = Region(regionName: regionName, countriesList: regionCountries)
+//
+//            default:
+//                print("None")
+//            }
+        
+    }
+    print(regionCountries)
+    return regionCountries
+}
