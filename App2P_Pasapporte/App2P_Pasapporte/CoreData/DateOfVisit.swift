ed0bc3246606ca8954af3261e7eb2cd4c9972f29
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-07 17:31:04 -0700

    Adds DateOfVisit object

---
+++ App2P_Pasapporte/App2P_Pasapporte/CoreData/DateOfVisit.swift
@@ -0,0 +1,16 @@
+//
+//  DateOfVisit.swift
+//  App2P_Pasapporte
+//
+//  Created by Enrique Diaz de Leon Hicks on 5/7/21.
+//
+
+import SwiftUI
+
+struct DateOfVisit: Identifiable {
+    
+    var id = UUID()
+    var country_name: String
+    var date_of_visit: Date
+    
+}
