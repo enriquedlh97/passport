ce3de13d5bdf10ad2f7d50db12b19c871ba6f4fd
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-07 17:41:25 -0700

    Adds dateOfVisit data model

---
+++ App2P_Pasapporte/DateOfVisit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DateOfVisit+CoreDataClass.swift
+//  App2P_Pasapporte
+//
+//  Created by user189351 on 5/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DateOfVisit)
+public class DateOfVisitInfo: NSManagedObject {
+
+}
